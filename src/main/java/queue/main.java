694e6f746da9673b61d0d3a146ad45bb26acbb5a
Parsa.zn <Parsaeagle@gmail.com>
2022-03-22 09:03:37 +0100

    Detected some errors

--- src/main/java/queue/main.java
+++ src/main/java/queue/main.java
@@ -8,10 +8,10 @@ public class main {
         dequeNodeFirst = new DequeNode<String>("First", null, null);
         dequeNodeLast = new DequeNode<String>("Last", null, null);
         doubleLinkedListQueue.appendLeft(dequeNodeFirst);
-        doubleLinkedListQueue.append(dequeNode);
-        doubleLinkedListQueue.append(dequeNodeLast);
+        doubleLinkedListQueue.appendLeft(dequeNode);
+        doubleLinkedListQueue.appendLeft(dequeNodeLast);
         System.out.println(doubleLinkedListQueue.peekFirst().getItem());
-        System.out.println(doubleLinkedListQueue.peekFirst().getNext().getItem());
+        System.out.println(doubleLinkedListQueue.peekFirst().getNext().getItem()); //here appears an error
         System.out.println(doubleLinkedListQueue.peekFirst().getNext().getNext().getItem());
         System.out.println(doubleLinkedListQueue.peekFirst().getPrevious().getItem());
         System.out.println(doubleLinkedListQueue.peekFirst().getPrevious().getPrevious().getItem());
